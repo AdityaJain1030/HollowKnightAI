bb2b7500cec3032d821cce3b4009cca52f17959b
AdityaJain1030 <50159919+AdityaJain1030@users.noreply.github.com>
2023-03-09 17:21:56 -0800

    Inital commit

---
+++ Mod/HollowKnightAI.cs
@@ -0,0 +1,31 @@
+﻿using Modding;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UObject = UnityEngine.Object;
+
+namespace HollowKnightAI
+{
+    internal class HollowKnightAI : Mod
+    {
+        internal static HollowKnightAI Instance { get; private set; }
+
+        public HollowKnightAI() : base("HollowKnightAI") { }
+
+        public override string GetVersion()
+        {
+            return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        }
+
+        public override void Initialize()
+        {
+            Log("Initializing");
+
+            Instance = this;
+
+            Log("Initialized");
+        }
+    }
+}
---
+++ Mod/ReadMe.md
@@ -0,0 +1,3 @@
+# HollowKnightAI
+
+An AI that learns to play hollow knight
